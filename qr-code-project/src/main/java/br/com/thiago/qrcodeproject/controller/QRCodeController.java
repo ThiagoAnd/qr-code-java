29a0db68c3d71e2c3c5148fdf80ff63aede7b16b
ThiagoAnd <thiago.andrade.it@outlook.com>
2021-09-29 02:10:04 -0300

    Update project

--- qr-code-project/src/main/java/br/com/thiago/qrcodeproject/controller/QRCodeController.java
+++ qr-code-project/src/main/java/br/com/thiago/qrcodeproject/controller/QRCodeController.java
@@ -2,39 +2,13 @@ package br.com.thiago.qrcodeproject.controller;
 
 import br.com.thiago.qrcodeproject.service.QRCodeGeneratorService;
 import com.google.zxing.WriterException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
 public class QRCodeController {
-
-    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QdddRCode.png";
-
-
-    @GetMapping(value = "/genrateAndDownloadQRCode/{codeText}/{width}/{height}")
-    public void download(
-            @PathVariable("codeText") String codeText,
-            @PathVariable("width") Integer width,
-            @PathVariable("height") Integer height) throws IOException, WriterException {
-       // codeText ="https://apps.apple.com/br/app/banco-bv/id1494202346";
-        //codeText = "https://play.google.com/store/apps/details?id=com.votorantim.bvpd&hl=pt_BR&gl=US";
-       // codeText="https://apps.apple.com/br/app/banco-ita%C3%BA/id474505665";
-        codeText ="Testandooo";
-        QRCodeGeneratorService.generateQRCodeImage(codeText, width, height, QR_CODE_IMAGE_PATH);
-    }
-
-    @GetMapping(value = "/genrateQRCode/{codeText}/{width}/{height}")
-    public ResponseEntity<byte[]> generateQRCode(
-            @PathVariable("codeText") String codeText,
-            @PathVariable("width") Integer width,
-            @PathVariable("height") Integer height) throws IOException, WriterException {
-        return ResponseEntity.status(HttpStatus.OK).body(QRCodeGeneratorService.getQRCodeImage(codeText, width, height));
-    }
 
     @GetMapping("/gerarStringQrCode")
     public String generateStringQrCode() throws IOException, WriterException {
@@ -45,6 +19,18 @@ public class QRCodeController {
     @GetMapping("/gerarUrlQrCode")
     public String generateUrlQrCode() throws IOException, WriterException {
         String image = QRCodeGeneratorService.generateUrlQrCode();
+        return "A imagem foi criada em: "+image;
+    }
+
+    @GetMapping("/gerarWifiQrCode")
+    public String generateWifiQrCode() throws IOException, WriterException {
+        String image = QRCodeGeneratorService.generateWifiQrCode();
+        return "A imagem foi criada em: "+image;
+    }
+
+    @GetMapping("/gerarCallQrCode")
+    public String generateCallQrCode() throws IOException, WriterException {
+        String image = QRCodeGeneratorService.generateLigacaoQrCode();
         return "A imagem foi criada em: "+image;
     }
 }
--- qr-code-project/src/main/java/br/com/thiago/qrcodeproject/service/QRCodeGeneratorService.java
+++ qr-code-project/src/main/java/br/com/thiago/qrcodeproject/service/QRCodeGeneratorService.java
@@ -1,17 +1,17 @@
 package br.com.thiago.qrcodeproject.service;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import lombok.experimental.UtilityClass;
+import com.google.zxing.client.result.VCardResultParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 @UtilityClass
 public class QRCodeGeneratorService {
@@ -20,6 +20,9 @@ public class QRCodeGeneratorService {
     private final int HEIGHT = 650;
     private final String QR_CODE_STRING_IMAGE = "./src/main/resources/QrCodeStringImage.png";
     private final String QR_CODE_URL_IMAGE = "./src/main/resources/QrCodeUrlImage.png";
+    private final String QR_CODE_WIFI_IMAGE = "./src/main/resources/QrCodeWifiImage.png";
+    private final String QR_CODE_CALL_IMAGE = "./src/main/resources/QrCodeCallImage.png";
+    private final String QR_CODE_VCARD_IMAGE = "./src/main/resources/QrCodeVCardImage.png";
 
 
 
@@ -32,27 +35,6 @@ public class QRCodeGeneratorService {
         return new File(QR_CODE_STRING_IMAGE).getAbsolutePath();
     }
 
-    public  void generateQRCodeImage(String text, int width, int height, String filePath)
-            throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-
-        Path path = FileSystems.getDefault().getPath(filePath);
-        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-
-    }
-
-
-    public  byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-
-        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
-        byte[] pngData = pngOutputStream.toByteArray();
-        return pngData;
-    }
-
     public  String generateUrlQrCode() throws WriterException, IOException {
         var urlAndroid = "https://play.google.com/store/apps/details?id=com.votorantim.bvpd&hl=pt_BR&gl=US";
         var urlIphone = "https://apps.apple.com/br/app/banco-ita%C3%BA/id474505665";
@@ -61,5 +43,34 @@ public class QRCodeGeneratorService {
         Path path = FileSystems.getDefault().getPath(QR_CODE_URL_IMAGE);
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
         return new File(QR_CODE_URL_IMAGE).getAbsolutePath();
+    }
+
+    public static String generateWifiQrCode() throws IOException, WriterException {
+        var wifiLogin = "WIFI:T:WPA;S:NOMEREDE;P:SENHA;;";
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(wifiLogin, BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
+        Path path = FileSystems.getDefault().getPath(QR_CODE_WIFI_IMAGE);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+        return new File(QR_CODE_WIFI_IMAGE).getAbsolutePath();
+    }
+
+    public static String generateLigacaoQrCode() throws IOException, WriterException {
+        var ligacao = " 4199771-9771\nThiago de Andrade";
+        var ligacaoComum = "41997713790";
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(ligacao, BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
+        Path path = FileSystems.getDefault().getPath(QR_CODE_CALL_IMAGE);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+        return new File(QR_CODE_CALL_IMAGE).getAbsolutePath();
+    }
+
+    public static String generateVCardQrCode() throws IOException, WriterException {
+        var ligacao = " 4199771-9771\nThiago de Andrade";
+        var ligacaoComum = "41997713790";
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(ligacao, BarcodeFormat.QR_CODE, WIDTH, HEIGHT);
+        Path path = FileSystems.getDefault().getPath(QR_CODE_CALL_IMAGE);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+        return new File(QR_CODE_CALL_IMAGE).getAbsolutePath();
     }
 }
