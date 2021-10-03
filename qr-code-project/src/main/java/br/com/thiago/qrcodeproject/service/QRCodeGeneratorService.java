f31d089175684176307d96905e9dc2b76485114d
ThiagoAnd <thiago.andrade.it@outlook.com>
2021-10-03 17:34:55 -0300

    Description: Update project

--- qr-code-project/src/main/java/br/com/thiago/qrcodeproject/service/QRCodeGeneratorService.java
+++ qr-code-project/src/main/java/br/com/thiago/qrcodeproject/service/QRCodeGeneratorService.java
@@ -4,39 +4,33 @@ import br.com.thiago.qrcodeproject.util.QrCodeMapper;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.client.result.CalendarParsedResult;
-import com.google.zxing.client.result.TelParsedResult;
-import com.google.zxing.client.result.TelResultParser;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import org.springframework.format.datetime.DateFormatter;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 @Service
 public class QRCodeGeneratorService {
 
-    private final int WIDTH = 650;
-    private final int HEIGHT = 650;
-    private final String QR_CODE_STRING_IMAGE = "./src/main/resources/QrCodeStringImage.png";
-    private final String QR_CODE_URL_IMAGE = "./src/main/resources/QrCodeUrlImage.png";
-    private final String QR_CODE_WIFI_IMAGE = "./src/main/resources/QrCodeWifiImage.png";
-    private final String QR_CODE_CALL_IMAGE = "./src/main/resources/QrCodeCallImage.png";
-    private final String QR_CODE_EMAIL_IMAGE = "./src/main/resources/QrCodeEmailImage.png";
-    private final String QR_CODE_VCARD_IMAGE = "./src/main/resources/QrCodeVCardImage.png";
-    private final String QR_CODE_SMS_IMAGE = "./src/main/resources/QrCodeSmsImage.png";
-    private final String QR_CODE_MAP_IMAGE = "./src/main/resources/QrCodeMapImage.png";
-    private final String QR_CODE_CALENDAR_IMAGE = "./src/main/resources/QrCodeCalendarImage.png";
+    private static final int WIDTH = 650;
+    private static final int HEIGHT = 650;
+    private static final String QR_CODE_STRING_IMAGE = "./src/main/resources/QrCodeStringImage.png";
+    private static final String QR_CODE_URL_IMAGE = "./src/main/resources/QrCodeUrlImage.png";
+    private static final String QR_CODE_WIFI_IMAGE = "./src/main/resources/QrCodeWifiImage.png";
+    private static final String QR_CODE_CALL_IMAGE = "./src/main/resources/QrCodeCallImage.png";
+    private static final String QR_CODE_EMAIL_IMAGE = "./src/main/resources/QrCodeEmailImage.png";
+    private static final String QR_CODE_VCARD_IMAGE = "./src/main/resources/QrCodeVCardImage.png";
+    private static final String QR_CODE_SMS_IMAGE = "./src/main/resources/QrCodeSmsImage.png";
+    private static final String QR_CODE_MAP_IMAGE = "./src/main/resources/QrCodeMapImage.png";
+    private static final String QR_CODE_CALENDAR_IMAGE = "./src/main/resources/QrCodeCalendarImage.png";
 
 
     public String generateStringQrCode() throws WriterException, IOException {
@@ -77,45 +71,17 @@ public class QRCodeGeneratorService {
     }
 
     public String generateCalendarQrCode() throws IOException, WriterException {
-    var x = LocalDate.parse("04/10/2021", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        System.out.println(x);
-        var y = LocalTime.parse("12:30",DateTimeFormatter.ofPattern("HH:mm"));
-        System.out.println(y);
-
-        String evento = "BEGIN:VEVENT\n" +
-                "SUMMARY:Testando o titulo dddo calendario\n" +
-                "DESCRIPTION:Teste de descrição de calendario\n"+
-                "CLASS:PRIVATE\n" +
-                "CATEGORIES:MEETING\n" +
-                "ORGANIZER:mailto:jane_doe@example.com\n" +
-                "ATTENDEE:mailto:john_public@example.com\n"+
-                "URL:http://github/thiagoand\n"+
-                "DTSTART:20180601T070000Z\n" +
-                "DTEND:20180831T070000Z\n" +
-                "END:VEVENT\n";
-
-
-
-
-
-        String calendar = "BEGIN:VALARM;" +
-                "TRIGGER:-PT1440M;" +
-                "ACTION:DISPLAY;" +
-                "DESCRIPTION:Reminder;" +
-                "END:VALARM";
-        return generateBaseQrCode(evento, QR_CODE_CALENDAR_IMAGE);
+        String event = QrCodeMapper.simpleCalendarParser("Reunião", "Descrição", dateFormat("04/07/2023", "14:29"), dateFormat("04/07/2023", "15:47"));
+        return generateBaseQrCode(event, QR_CODE_CALENDAR_IMAGE);
     }
 
     public String generateVCardQrCode() throws IOException, WriterException {
-
         String vCard = QrCodeMapper.vCardParser("Thiago Andrade", "Google",
                 "Desenvolvedor", "+5541997719771",
                 "github/thiagoand", "s4lezardv@gmail.com",
                 "Rua Medico, numero 248,Curitiba,Parana");
-
         return generateBaseQrCode(vCard, QR_CODE_VCARD_IMAGE);
     }
-
 
     private String generateBaseQrCode(String conteudo, String arquivo) throws IOException, WriterException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
@@ -125,4 +91,8 @@ public class QRCodeGeneratorService {
         return new File(arquivo).getAbsolutePath();
     }
 
+    private String dateFormat(String dia, String hora) {
+        var data = LocalDateTime.of(LocalDate.parse(dia, DateTimeFormatter.ofPattern("dd/MM/yyyy")), LocalTime.parse(hora, DateTimeFormatter.ofPattern("HH:mm")));
+        return data.format(DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss"));
+    }
 }
--- qr-code-project/src/main/java/br/com/thiago/qrcodeproject/util/QrCodeMapper.java
+++ qr-code-project/src/main/java/br/com/thiago/qrcodeproject/util/QrCodeMapper.java
@@ -17,14 +17,14 @@ public class QrCodeMapper {
         return format(simpleEmailTemplate, email);
     }
 
-    public String normalEmailParser(String email, String assunto,String corpo) {
+    public String normalEmailParser(String email, String assunto, String corpo) {
         String normalEmailTemplate = "MAILTO:%s?SUBJECT=%s&BODY=%s";
-        return format(normalEmailTemplate, email, assunto,corpo);
+        return format(normalEmailTemplate, email, assunto, corpo);
     }
 
-    public String fowardEmailParser(String email, String copia1, String copia2, String copiaOculta){
+    public String fowardEmailParser(String email, String copia1, String copia2, String copiaOculta) {
         String mixedEmailTemplate = "MAILTO:%s?CC=%s,%s&BCC=%s";
-        return format(mixedEmailTemplate, email,copia1,copia2,copiaOculta);
+        return format(mixedEmailTemplate, email, copia1, copia2, copiaOculta);
     }
 
     public String smsParser(String fone, String texto) {
@@ -32,52 +32,45 @@ public class QrCodeMapper {
         return format(EncodingString, fone, texto);
     }
 
-    public String mapParser(String latitude,String longitude,String distancia){
+    public String mapParser(String latitude, String longitude, String distancia) {
         String mapTemplate = "GEO:%s,%s,%s";
-        return format(mapTemplate, latitude,longitude,distancia);
+        return format(mapTemplate, latitude, longitude, distancia);
     }
 
-    public String simpleCalendarParser(String titulo,String descricao,String inicio,String fim){
-
+    public String simpleCalendarParser(String titulo, String descricao, String inicio, String fim) {
         String simpleCalendarTemplate = "BEGIN:VEVENT\n" +
                                             "SUMMARY:%s\n" +
-                                            "DESCRIPTION:%s\n"+
+                                            "DESCRIPTION:%s\n" +
                                             "DTSTART:%s\n" +
                                             "DTEND:%s\n" +
                                         "END:VEVENT\n";
-
-        return format(simpleCalendarTemplate,titulo,descricao,inicio,fim);
-
+        return format(simpleCalendarTemplate, titulo, descricao, inicio, fim);
     }
 
-    public String multiCalendarParser(String titulo,String descricao,String inicio,String fim){
-        String evento = "BEGIN:VEVENT\n" +
-                "SUMMARY:Testando o titulo dddo calendario\n" +
-                "DESCRIPTION:Teste de descrição de calendario\n"+
-                "CLASS:PRIVATE\n" +
-                "CATEGORIES:MEETING\n" +
-                "ORGANIZER:mailto:jane_doe@example.com\n" +
-                "ATTENDEE:mailto:john_public@example.com\n"+
-                "URL:http://github/thiagoand\n"+
-                "DTSTART:20180601T070000Z\n" +
-                "DTEND:20180831T070000Z\n" +
-                "END:VEVENT\n";
-        return null;
+    public String multiCalendarParser(String titulo, String descricao, String url, String organizador, String convidado, String inicio, String fim) {
+        String multiCalendarTemplate = "BEGIN:VEVENT\n" +
+                                            "SUMMARY:%s\n" +
+                                            "DESCRIPTION:%s\n" +
+                                            "ORGANIZER:mailto:%s\n" +
+                                            "ATTENDEE:mailto:%s\n" +
+                                            "URL:%s\n" +
+                                            "DTSTART:%s\n" +
+                                            "DTEND:%s\n" +
+                                        "END:VEVENT\n";
+        return format(multiCalendarTemplate, titulo, descricao, url, organizador, convidado, inicio, fim);
     }
 
     public String vCardParser(String nome, String empresa, String cargo, String telefone, String site, String email, String endereco) {
-        String vCardTemplate = "BEGIN:VCARD\n"
-                + "VERSION:4.0\n"
-                + "N:%s\n"
-                + "ORG:%s\n"
-                + "TITLE:%s\n"
-                + "TEL:%s\n"
-                + "URL:%s\n"
-                + "EMAIL:%s\n"
-                + "ADR:%s\n"
-                + "END:VCARD\n";
+        String vCardTemplate = "BEGIN:VCARD\n"+
+                                    "VERSION:4.0\n"+
+                                    "N:%s\n"+
+                                    "ORG:%s\n"+
+                                    "TITLE:%s\n"+
+                                    "TEL:%s\n"+
+                                    "URL:%s\n"+
+                                    "EMAIL:%s\n"+
+                                    "ADR:%s\n"+
+                                "END:VCARD\n";
         return format(vCardTemplate, nome, empresa, cargo, telefone, site, email, endereco);
     }
-
-
 }
